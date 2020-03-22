db05296d2e503cc96ae5ee5874c1a29e5a63eed2
Jarosław Wiosna <jaroslaw.wiosna.github@gmail.com>
2020-03-22 15:59:07 +0100

    Proof of concept is finished

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 all: free-average
 
 free-average: main.cpp Makefile
-	g++ -std=c++17 -o free-average main.cpp
+	g++ -std=c++17 -ggdb -o free-average main.cpp
 
 install:
 	echo "TODO: make install"
--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <cstdio>
-#include <cassert>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -13,6 +12,8 @@
 const int times_per_sec = 2;
 const int dt = 1000 / times_per_sec;
 
+struct String_view;
+
 // Ring buffer buf we never pop
 template <typename T, int capacity>
 struct Ring_buffer {
@@ -21,7 +22,7 @@ struct Ring_buffer {
     bool is_full;
 
     void push(T x);
-    std::optional<T> avg();
+    String_view avg();
 };
 
 template <typename T, int capacity>
@@ -33,45 +34,213 @@ void Ring_buffer<T, capacity>::push(T x) {
     buf[begin++] = x;
 }
 
+struct String_view {
+    size_t len;
+    const char* str;
+
+    String_view trim_begin(void) const
+    {
+        String_view result = *this;
+
+        while (result.len != 0 && isspace(*result.str)) {
+            result.str  += 1;
+            result.len -= 1;
+        }
+        return result;
+    }
+
+    String_view trim_end(void) const
+    {
+        String_view result = *this;
+
+        while (result.len != 0 && isspace(*(result.str + result.len - 1))) {
+            result.len -= 1;
+        }
+        return result;
+    }
+
+    String_view trim(void) const
+    {
+        return trim_begin().trim_end();
+    }
+
+    void chop(size_t n)
+    {
+        if (n > len) {
+            len = 0;
+        } else {
+            str += n;
+            len -= n;
+        }
+    }
+
+    String_view chop_by_delim(char delim)
+    {
+        size_t i{};
+        while (i < len && str[i] != delim) {
+            i++;
+        }
+        String_view result{i, str};
+        chop(i + 1);
+
+        return result;
+    }
+
+    String_view chop_word(void)
+    {
+        *this = trim_begin();
+
+        size_t i{};
+        while (i < len && !isspace(str[i])) i++;
+
+        String_view result {i, str};
+
+        len -= i;
+        str += i;
+
+        return result;
+    }
+
+    template <typename Int>
+    std::optional<Int> chop_int() const
+    {
+        Int sign {1};
+        Int number {};
+        String_view view = *this;
+
+        if (view.len == 0) {
+            return {};
+        }
+
+        if (*view.str == '-') {
+            sign = -1;
+            view.chop(1);
+        }
+
+        while (view.len) {
+            if (!isdigit(*view.str)) {
+                return {};
+            }
+            number = number * 10 + (*view.str - '0');
+            view.chop(1);
+        }
+
+        return {sign * number};
+    }
+};
+
+String_view operator ""_sv (const char *str, size_t len) {
+    String_view result;
+    result.str = str;
+    result.len = len;
+    return result;
+}
+
 template <typename T, int capacity>
-std::optional<T> Ring_buffer<T, capacity>::avg() {
+String_view Ring_buffer<T, capacity>::avg() {
     if (!is_full) {
-        return {};
+        return ("--.--"_sv);
     }
     T acc{};
     for (int i{}; i < capacity; ++i) {
         acc += buf[i];
     }
-    return acc / capacity;
+    static char buf[10];
+    sprintf(buf, "%2.2f", acc / capacity);
+    String_view result{};
+    result.str = buf;
+    result.len = 5;
+    return result;
 }
 
-char buf[8192];
-int fd;
+std::optional<String_view> meminfo_to_sv() {
+    String_view result{};
+
+    size_t n = 0;
+    FILE *f = fopen("/proc/meminfo", "rb");
+    if (!f) {
+        return {};
+    }
+
+    int code = fseek(f, 0, SEEK_END);
+    if (code < 0) {
+        return {};
+    }
+
+    result.len = (size_t) 140; // b'cuz it 3 lines fit in 140 char...
+
+    code = fseek(f, 0, SEEK_SET);
+    if (code < 0) {
+        return {};
+    }
+
+    char *buffer = new char[result.len];
+    if (!buffer) {
+        return {};
+    }
+
+    n = fread(buffer, 1, result.len, f);
+    if (n != result.len) {
+        return {};
+    }
+
+    result.str = buffer;
+    free(buffer);
+
+    return result;
+}
+
+struct Memory {
+    unsigned long total;
+    unsigned long available;
+};
+
+std::optional<Memory> current_memory() {
+    Memory result;
+    auto meminfo = meminfo_to_sv();
+    if (!meminfo.has_value()) {
+        return {};
+    }
+    auto total = meminfo.value().chop_by_delim('\n');
+
+    total.chop_word();
+    total = total.trim();
+    result.total = total.chop_word().chop_int<unsigned long>().value();
+
+    meminfo.value().chop_by_delim('\n');
+    auto available = meminfo.value().chop_by_delim('\n');
+
+    available.chop_word();
+    available = available.trim();
+    result.available = available.chop_word().chop_int<unsigned long>().value();
+    return result;
+}
 
 int main() {
-    float i{};
     setbuf(stdout, nullptr);
 
     Ring_buffer<float, times_per_sec> one_second{};
     Ring_buffer<float, times_per_sec * 60> one_minute{};
     Ring_buffer<float, times_per_sec * 60 * 2> two_minutes{};
-
     while (1) {
-        fd = open("/proc/meminfo", O_RDONLY);        
-        int n = read(fd, buf, sizeof (buf - 1));
-        buf[n] = '\0';
+        std::optional<Memory> memory = current_memory();
+        if (!memory.has_value()) {
+        printf("                                                    \r"); 
+        printf("  :-(                                               \n"); 
+            continue;
+        }
+        
+        auto i = 100.0 * memory.value().available / memory.value().total;
 
         one_second.push(i);
         one_minute.push(i);
         two_minutes.push(i);
 
-        i = (i < 15) ? i+1 : 0;
-       
         printf("                                                    \r"); 
-        printf(" free-average\t%3.2f\t%3.2f\t%3.2f\r", 
-                one_second.avg().value_or(0.0),
-                one_minute.avg().value_or(0.0),
-                two_minutes.avg().value_or(0.0));
+        printf(" free-average\t%.*s\t%.*s\t%.*s\r", 
+                one_second.avg().len, one_second.avg().str, 
+                one_minute.avg().len, one_minute.avg().str,
+                two_minutes.avg().len, two_minutes.avg().str);
         std::this_thread::sleep_for(std::chrono::milliseconds(dt));
     }
 }
